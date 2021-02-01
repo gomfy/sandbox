dfb5c2dc24a0173b33e4a148923e8adaa749d349
Gyorgy Matyasfalvi <matyasfalvi@ampl.com>
2021-02-01 14:53:30 -0800

    Prepping for SO question.

--- cmake/main.c
+++ cmake/main.c
@@ -4,10 +4,6 @@ extern void foo();
 extern int sv;
 
 int main() {
-    if(sv > 0)
-        printf("\nsv is positive\n");
-    else
-        printf("\nsv is negative\n");
     foo();
     return 0;
 }
