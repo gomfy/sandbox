a887421264aef0a72b392136add236cc761841ec
Gyorgy Matyasfalvi <matyasfalvi@ampl.com>
2021-02-04 15:45:09 -0800

    Minor print mod.

--- cmake/bar.c
+++ cmake/bar.c
@@ -4,9 +4,9 @@ extern "C" int g1;
 
 void bar(int x) {
 	if(x<g1) {
-		printf("%d is less than %d/n",x,g1);
+		printf("%d is less than %d\n",x,g1);
 	}
 	else {	
-	printf("%d is greater or equal than %d/n",x,g1);
+		printf("%d is greater or equal than %d\n",x,g1);
 	}	
 }
