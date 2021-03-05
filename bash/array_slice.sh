5912002d0e9f4aea0e746c5896fa46f082d23f39
Gyorgy Matyasfalvi <matyasfalvi@ampl.com>
2021-03-05 17:18:09 -0500

    G.M. -- testing array slicing in bash.

--- bash/array_slice.sh
+++ bash/array_slice.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
-a=( "fjklsj" "kldj" "123" 39874 993 )
-printf "\nElements of array 'a':\n"
-printf "%s\n" "${a[@]}"
-
-a23=( ${a[@]:2} )
-printf "\nElements of array 'a23':\n"
-printf "%s\n" "${a23[@]}"
-
 defaultIFS=$IFS
 IFS=$'\n'
-t=($(awk '$1=="text" { printf("%s\n", $0) }' "$1"))
-printf "\nElements of array 't':\n"
+t=($(awk '{ printf("%s\n", $0) }' "$1"))
+printf "\nArray 't' has ${#t[@]} elements\n"
+printf "Elements of array 't':\n"
 printf "%s\n" "${t[@]}"
 
-t23=( ${t[@]:2} )
-printf "\nElements of array 't23':\n"
-printf "%s\n" "${t23[@]}"
+t45=( ${t[@]:4} )
+printf "\nArray 't45' has ${#t45[@]} elements\n"
+printf "Elements of array 't45':\n"
+printf "%s\n" "${t45[@]}"
 
