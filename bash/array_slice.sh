d2044a29ee2fb3f25c60745f909bc98ae3694d42
Gyorgy Matyasfalvi <matyasfalvi@ampl.com>
2021-03-05 18:37:30 -0500

    G.M. -- testing array slicing in bash.

--- bash/array_slice.sh
+++ bash/array_slice.sh
@@ -7,7 +7,7 @@ printf "\nArray 't' has ${#t[@]} elements\n"
 printf "Elements of array 't':\n"
 printf "%s\n" "${t[@]}"
 
-t45=( ${t[@]:4} )
+t45=( "${t[@]:4}" )
 printf "\nArray 't45' has ${#t45[@]} elements\n"
 printf "Elements of array 't45':\n"
 printf "%s\n" "${t45[@]}"
