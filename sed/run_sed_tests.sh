4753cf18ca2fe808d79167db58160d3e891dfdbd
Gyorgy Matyasfalvi <matyasfalvi@ampl.com>
2021-03-31 09:34:15 -0400

    Test sed xpl-ly.

--- sed/run_sed_tests.sh
+++ sed/run_sed_tests.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-cat file.1 | sed --regexp-extended 's/([0-9]+)([^0-9]+iterations)/XXX\2/' 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sed_cmd=sed
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sed_cmd=gsed
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    sed_cmd=sed
+else
+        print_error
+        printf "MESSAGE: Unable to detect host OS type...\n"
+        exit $EXIT_FAILURE
+fi
+
+cat file.1 | $sed_cmd --regexp-extended 's/([0-9]+)([^0-9]+iterations)/XXX\2/' 
+
