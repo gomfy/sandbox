10d6a7071178f4116331864638236bf93a200aa8
Marcos <marcosdominguez.ampl@gmail.com>
2021-04-19 19:06:17 +0200

    Path conversions

---
+++ path_conversion/converting_paths.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Cygpath tests based on:
+# https://cygwin.com/cygwin-ug-net/cygpath.html
+
+if [[ ! "$OSTYPE" == "msys"* ]]; then
+	echo "No test, OS = $OSTYPE"
+	exit 0
+fi
+
+echo "### BEGIN TEST ###"
+
+apath1=/home/invented/unix/path.file
+apath2="/home/invented/unix/path.aaa"
+rpath1=../mypath
+rpath2="../../path?nl"
+
+echo "Current dir: $(pwd)"
+echo "Current dir (Windows): $(cygpath -w `pwd`)"
+
+echo "$apath1 conversion to Windows is:"
+echo "$(cygpath -w apath1)"
+echo "Now absolute: $(cygpath -w -a apath1)"
+
+echo "$apath2 conversion to Windows is:"
+newpath=`cygpath -w apath2`
+echo "$newpath"
+newpath=`cygpath -w -a apath2`
+echo "Now absolute: $newpath"
+
+echo "$rpath1 conversion to Windows is:"
+echo "$(cygpath -w -m rpath1)"
+echo "Now absolute: $(cygpath -w -a -m rpath1)"
+
+echo "$rpath2 conversion to Windows is:"
+echo "$(cygpath -w -m rpath2)"
+echo "Now absolute: $(cygpath -w -a -m rpath2)"
+
+echo "### END TEST ###"
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
